48bf8394ca110c995721e7c5b163fa0a8adcbc72
riasad-alvi <alvi101.cs@gmail.com>
2024-07-30 17:02:24 +0600

    variables, functions and datatypes of JS added

---
+++ 1.Variables,Functions,DataTypes/basics.js
@@ -0,0 +1,95 @@
+// Three ways to declare a variable in JS :
+
+//way 1 : using var keyword :
+
+var score = 99;
+
+var score = 5;
+
+//way 2 : using let keyword :
+
+let age = 24;
+let flag = true;
+
+//way 3 : using const keyword :
+
+const x = 77;
+
+console.log("--------------------------");
+
+// Functions :
+
+console.log(99);
+console.log("Hola");
+console.log(age, x, 88, "U Cant See Me");
+
+console.log("--------------------------");
+
+// Data-Types in JS :
+
+// numbers :
+let y = 9;
+
+//Strings :
+
+console.log("Hehe");
+console.log("Wow:)");
+console.log(`Attack them ! `);
+
+var str = `hola`;
+
+//boolean :
+
+var cond = false;
+
+//undefined :
+
+var status = undefined;
+
+console.log("--------------------------");
+
+// Other Data Types :
+
+var name = "Alvi";
+var sec;
+var height = null;
+var colour = undefined;
+var isAStudent = true;
+
+console.log(name);
+console.log(sec);
+console.log(height);
+console.log(colour);
+console.log(isAStudent);
+
+// Object :
+console.log("--------------------------");
+
+user = {
+  name: " Alvi",
+  age: 24,
+  sec: "B",
+  gender: `Male`,
+};
+
+console.log(user);
+
+console.log("--------------------------");
+// Special Characters :
+
+let text = "How are \nyou doing ";
+let text2 = "what \t the heck are u ";
+
+console.log(text);
+console.log(text2);
+
+console.log("--------------------------");
+
+// this is a single line comment
+
+/* 
+
+This is an example of
+multiple line comment 
+
+*/
