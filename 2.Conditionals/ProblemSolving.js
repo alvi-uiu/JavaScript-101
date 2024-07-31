5f4287cf27aace6274d179cd54d15f5e37154b55
riasad-alvi <alvi101.cs@gmail.com>
2024-07-31 09:15:13 +0600

    another triangle problem added

--- 2.Conditionals/ProblemSolving.js
+++ 2.Conditionals/ProblemSolving.js
@@ -29,3 +29,27 @@ let x = -9999999,
 if (x < y && x < z) console.log(x);
 else if (y < x && y < z) console.log(y);
 else console.log(z);
+
+console.log("----------------------------------");
+
+// Is a Valid Triangle :
+
+let e = 7,
+  f = 10,
+  g = 13;
+
+if (e + f > g && e + g > f && f + g > e) console.log("valid triangle");
+else console.log("not a valid triangle ");
+
+console.log("----------------------------------");
+
+// Given length of 3 axis of a triangle , tell if the triangle
+// scalene, equilateral , isosceles :
+
+let i = 7,
+  j = 9,
+  k = 77;
+
+if (i == j && i == k && j == k) console.log("equilateral");
+else if (i != j && i != k && j != k) console.log("scalene");
+else console.log("isosceles");
