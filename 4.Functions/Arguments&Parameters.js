dcd01b36f8d880cf2a2b88c3f8ca6ccf9ebf2d00
riasad-alvi <alvi101.cs@gmail.com>
2024-08-01 10:38:26 +0600

    Arguments and Parameters

---
+++ 4.Functions/Arguments&Parameters.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+  let res = x + y;
+  return res;
+}
+
+let a = 99;
+let b = -88;
+
+let result = add(a, b);
+
+console.log(result);
+
+//it can also be written like this :
+
+console.log(add(77, 99));
